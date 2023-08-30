88c32ad8177a5a999077fce25ec910fa3d4313e2
Mathis Ribémont <mathisribemont@gmail.com>
2023-08-30 15:26:10 +0200

    rename MathSquare to Print

--- src/environnement/GlobalContext.java
+++ src/environnement/GlobalContext.java
@@ -3,13 +3,9 @@ package environnement;
 import calculator.Context;
 import calculator.nodes.Block;
 import calculator.nodes.NumberValue;
-import environnement.functions.MathSquare;
-import io.IReader;
+import environnement.functions.Print;
 import io.IWriter;
-import io.StdReader;
 import io.StdWriter;
-
-import java.util.LinkedList;
 
 public class GlobalContext extends Context {
     public GlobalContext(){
@@ -17,7 +13,7 @@ public class GlobalContext extends Context {
         setVariable("pi", new NumberValue(Math.PI));
 
         IWriter writer = new StdWriter();
-        Block print = new MathSquare(writer);
+        Block print = new Print(writer);
         setVariable("print", print);
     }
 }
--- src/environnement/functions/Print.java
+++ src/environnement/functions/Print.java
@@ -5,21 +5,19 @@ import calculator.exceptions.NotAFunctionException;
 import calculator.exceptions.NotInterpretableException;
 import calculator.nodes.Block;
 import calculator.nodes.Node;
-import calculator.nodes.NullValue;
 import calculator.nodes.NumberValue;
-import io.IReader;
 import io.IWriter;
 
 import java.util.List;
 
-public class MathSquare extends Block {
+public class Print extends Block {
     IWriter writer;
-    public MathSquare(List<String> parameterNames, IWriter writer) {
+    public Print(List<String> parameterNames, IWriter writer) {
         super(parameterNames);
         this.writer = writer;
     }
 
-    public MathSquare(IWriter writer){
+    public Print(IWriter writer){
         this(null, writer);
         addParameterName("message");
     }
